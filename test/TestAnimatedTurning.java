836a152392afd707aa76c18bdad2e861b7fa0458
Oshgnacknak <osh@oshgnacknak.de>
2021-09-08 11:24:54 +0200

    Move after turning

--- test/TestAnimatedTurning.java
+++ test/TestAnimatedTurning.java
@@ -21,16 +21,15 @@ public class TestAnimatedTurning implements Scene {
     @Override
     public void run(World world) {
         var robots = List.of(
-            world.newRobot(0, 0, Direction.NORTH, 0),
-            world.newRobot(1, 1, Direction.EAST, 0),
-            world.newRobot(2, 2, Direction.SOUTH, 0),
-            world.newRobot(3, 3, Direction.WEST, 0));
+            world.newRobot(4, 4, Direction.NORTH, 0),
+            world.newRobot(5, 5, Direction.EAST, 0),
+            world.newRobot(6, 6, Direction.SOUTH, 0),
+            world.newRobot(7, 7, Direction.WEST, 0));
 
         while (true) {
             for (var r : robots) {
                 r.turnLeft();
-                r.turnLeft();
-                r.turnLeft();
+                r.move();
             }
         }
     }
