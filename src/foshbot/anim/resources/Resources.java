a9d2b04aa0974711f76c6024150b16cb91373dc4
Oshgnacknak <osh@oshgnacknak.de>
2021-09-09 11:27:37 +0200

    Load coin image

--- src/foshbot/anim/resources/Resources.java
+++ src/foshbot/anim/resources/Resources.java
@@ -1,6 +1,7 @@
 package foshbot.anim.resources;
 
 
+import foshbot.anim.AnimatedCoinStack;
 import foshbot.anim.AnimatedRobot;
 
 import java.awt.*;
@@ -11,7 +12,9 @@ public class Resources {
     private static ResourceCache<Image> images;
 
     public static void loadAll() throws IOException {
-        images = new ImageResourceCache(AnimatedRobot.RESOURCE);
+        images = new ImageResourceCache(
+            AnimatedRobot.RESOURCE,
+            AnimatedCoinStack.RESOURCE);
     }
 
     public static ResourceCache<Image> getImages() {
