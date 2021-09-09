34e67c3d353a7ecedec5e92257b7ff5660bd0c08
Oshgnacknak <osh@oshgnacknak.de>
2021-09-09 11:20:39 +0200

    Don't allow setting position of if entities directly

--- src/foshbot/Entity.java
+++ src/foshbot/Entity.java
@@ -10,14 +10,6 @@ public abstract class Entity {
         this.y = y;
     }
 
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
     public int getX() {
         return this.x;
     }
