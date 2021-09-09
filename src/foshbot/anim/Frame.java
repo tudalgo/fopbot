93c6e184ed58eb2552a7bf0f5f480bf6759aa42e
Oshgnacknak <osh@oshgnacknak.de>
2021-09-09 11:14:27 +0200

    Allow panning and zooming of the world

--- src/foshbot/anim/Frame.java
+++ src/foshbot/anim/Frame.java
@@ -1,53 +1,73 @@
 package foshbot.anim;
 
 import foshbot.World;
+import foshbot.anim.paz.PanningAndZooming;
+import foshbot.anim.paz.PanningAndZoomingTarget;
+import foshbot.anim.paz.Vector;
 import foshbot.impl.Grid;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class Frame extends JFrame {
+public class Frame extends JFrame implements PanningAndZoomingTarget {
 
-    public static final int CELL_SIZE = 80;
-    public static final int CELL_PADDING = 20;
-    private static final int FRAME_DELAY = 1000 / 30;
+    public static final double CELL_SIZE = 120;
+    public static final double CELL_PADDING = 32;
+    private static final double  FRAME_DELAY = 1000.0 / 30;
 
     private final AnimatedWorld world;
+    private final PanningAndZooming panningAndZooming;
 
     public Frame(Grid grid) {
         this.world = new AnimatedWorld(grid);
 
-        setSize(world.getWidth() * CELL_SIZE, world.getHeight() * CELL_SIZE);
-        setPreferredSize(getSize());
+        this.panningAndZooming = new PanningAndZooming(this);
+        panningAndZooming.setMaxScale(4);
+        panningAndZooming.setMinScale(0.6);
+
+        addMouseListener(panningAndZooming);
+        addMouseMotionListener(panningAndZooming);
+        addMouseWheelListener(panningAndZooming);
 
         add(new Canvas(this::draw));
         pack();
-        setResizable(false);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        panningAndZooming.setMinTopLeft(new Vector(-CELL_SIZE, -CELL_SIZE));
+        panningAndZooming.setMaxTopLeft(new Vector(
+            CELL_SIZE * (grid.getWidth() - 1),
+            CELL_SIZE * (grid.getHeight() - 1)));
     }
 
     private void draw(Drawable d) {
-        drawGrid(d);
-        world.drawWalls(d);
-        world.drawEntities(d);
+        d.fill(Color.BLACK);
+        d.rect(0, 0, getWidth(), getHeight());
+
+        panningAndZooming.draw(d, () -> {
+            drawGrid(d);
+            world.drawWalls(d);
+            world.drawEntities(d);
+        });
     }
 
     private void drawGrid(Drawable d) {
+        var width = world.getWidth()*CELL_SIZE;
+        var height = world.getHeight()*CELL_SIZE;
+
         d.fill(Color.GRAY);
-        d.rect(0, 0, getWidth(), getHeight());
+        d.rect(0, 0, width, height);
 
         d.fill(Color.DARK_GRAY);
         d.strokeWeight(4);
-
         for (int i = 1; i < world.getWidth(); i++) {
             var x = i * CELL_SIZE;
-            d.line(x, 0, x, getHeight());
+            d.line(x, 0, x, world.getHeight()*CELL_SIZE);
         }
 
         for (int i = 1; i < world.getHeight(); i++) {
             var y = i * CELL_SIZE;
-            d.line(0, y, getWidth(), y);
+            d.line(0, y, world.getWidth()*CELL_SIZE, y);
         }
     }
 
@@ -76,4 +96,7 @@ public class Frame extends JFrame {
             last = current;
         }
     }
+
+    @Override
+    public void onMousePressed(int button, Vector v) {}
 }
