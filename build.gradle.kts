5ed3db9d88ba96342a58e8fb398c7a53ea934a6a
Alexander Staeding <alexanderstaeding@gmail.com>
2021-09-10 12:16:48 +0200

    Temporary code for publishing to mavenlocal

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,16 @@
 plugins {
   `java-library`
+  `maven-publish`
 }
 
 group = "org.sourcegrade"
 version = "0.1.0-SNAPSHOT"
+
+publishing {
+  publications {
+    create<MavenPublication>("maven") {
+      from(components["java"])
+      artifactId = "fopbot"
+    }
+  }
+}
