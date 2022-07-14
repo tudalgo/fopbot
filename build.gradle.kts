3c43b97fc2963d3ad84731c6f055be48bcf3db27
Alexander Staeding <alexanderstaeding@gmail.com>
2022-07-14 10:42:02 +0200

    Update to Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,6 @@ version = file("version").readLines().first()
 apply<FOPBotPublishPlugin>()
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
