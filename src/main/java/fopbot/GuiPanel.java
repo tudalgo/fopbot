5952df8c0b240e2795925925b1db3e7b8f7b26b1
Dustin Glaser <dustin.glaser@posteo.de>
2022-10-02 13:47:04 +0200

    add import for Graphics2D

--- src/main/java/fopbot/GuiPanel.java
+++ src/main/java/fopbot/GuiPanel.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Point;
 import java.awt.Toolkit;
