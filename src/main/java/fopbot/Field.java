f71f1008e29e2dc8de729636c628dcb3fccab835
Rdeisenroth <53945743+Rdeisenroth@users.noreply.github.com>
2023-05-17 19:37:08 +0200

    Add ability to change field colors (#37)

--- src/main/java/fopbot/Field.java
+++ src/main/java/fopbot/Field.java
@@ -1,5 +1,8 @@
 package fopbot;
 
+import org.jetbrains.annotations.Nullable;
+
+import java.awt.Color;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,6 +17,8 @@ public class Field {
      * Contains all entities that are on this field.
      */
     private final List<FieldEntity> entities;
+
+    private @Nullable Color fieldColor;
 
     /**
      * Constructs and initializes a field with no entities on it.
@@ -38,5 +43,25 @@ public class Field {
      */
     public List<FieldEntity> getEntities() {
         return entities;
+    }
+
+    /**
+     * Sets the background color of this {@link Field} to the specified color.
+     * <p>If the specified color is {@code null}, the background color of this {@link Field}
+     * will be reset to the default color.</p>
+     *
+     * @param fieldColor the new background color of this {@link Field}
+     */
+    public void setFieldColor(final @Nullable Color fieldColor) {
+        this.fieldColor = fieldColor;
+    }
+
+    /**
+     * Returns the background color of this {@link Field}.
+     *
+     * @return the background color of this {@link Field}
+     */
+    public @Nullable Color getFieldColor() {
+        return fieldColor;
     }
 }
