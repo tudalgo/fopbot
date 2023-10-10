63abbac2dae219f3f89f505244b8774c478672ae
Rdeisenroth <53945743+Rdeisenroth@users.noreply.github.com>
2023-10-10 18:44:37 +0200

    Direction improvements (#36)
    * Direction improvements
    
    * remove too many util methods

--- src/main/java/fopbot/Direction.java
+++ src/main/java/fopbot/Direction.java
@@ -1,5 +1,7 @@
 package fopbot;
 
+import java.util.Arrays;
+
 /**
  * Defines the (viewing) direction that an entity can have.
  */
@@ -7,17 +9,72 @@ public enum Direction {
     /**
      * The direction upwards or direction of view "north".
      */
-    UP,
+    UP(0, 1),
     /**
      * The direction to the right or direction of view "east".
      */
-    RIGHT,
+    RIGHT(1, 0),
     /**
      * The direction downwards or direction of view "south".
      */
-    DOWN,
+    DOWN(0, -1),
     /**
      * The direction to the left or direction of view "west".
      */
-    LEFT,
+    LEFT(-1, 0);
+    /**
+     * The x offset of the {@link Direction}.
+     */
+    final int dx;
+    /**
+     * The y offset of the {@link Direction}.
+     */
+    final int dy;
+
+    /**
+     * Creates a new direction.
+     *
+     * @param dx The x-offset of the {@link Direction}.
+     * @param dy The y-offset of the {@link Direction}.
+     */
+    Direction(final int dx, final int dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    /**
+     * Gets the x offset of the {@link Direction}.
+     *
+     * @return The x offset of the {@link Direction}.
+     */
+    public int getDx() {
+        return dx;
+    }
+
+    /**
+     * Gets the y offset of the {@link Direction}.
+     *
+     * @return The y offset of the {@link Direction}.
+     */
+    public int getDy() {
+        return dy;
+    }
+
+    /**
+     * Checks whether this direction is a horizontal direction.
+     *
+     * @return {@code true} if this direction is a horizontal direction.
+     */
+    public boolean isHorizontal() {
+        return dy == 0;
+    }
+
+    /**
+     * Checks whether this direction is a vertical direction.
+     *
+     * @return {@code true} if this direction is a vertical direction.
+     */
+    public boolean isVertical() {
+        return dx == 0;
+    }
 }
