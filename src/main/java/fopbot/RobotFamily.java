e8d5bfe8b0be6e864ac2f557013395a0544afb0d
Rdeisenroth <rdeisenroth@gmail.com>
2024-06-17 16:06:20 +0200

    give each RobotFamily a retrievable Color

--- src/main/java/fopbot/RobotFamily.java
+++ src/main/java/fopbot/RobotFamily.java
@@ -1,5 +1,7 @@
 package fopbot;
 
+import java.awt.Color;
+
 /**
  * An enumeration of robot families.
  * A family of robots is uniquely in terms of their appearance.
@@ -8,43 +10,66 @@ public enum RobotFamily {
     /**
      * A Blue Triangle robot.
      */
-    TRIANGLE_BLUE,
+    TRIANGLE_BLUE(Color.BLUE),
     /**
      * A Teal Square robot.
      */
-    SQUARE_AQUA,
+    SQUARE_AQUA(Color.CYAN),
     /**
      * A Black Square robot.
      */
-    SQUARE_BLUE,
+    SQUARE_BLUE(Color.BLUE),
     /**
-     *A Green Square robot.
+     * A Green Square robot.
      */
-    SQUARE_GREEN,
+    SQUARE_GREEN(Color.GREEN),
     /**
      * An Orange Square robot.
      */
-    SQUARE_ORANGE,
+    SQUARE_ORANGE(Color.ORANGE),
     /**
      * A Purple Square robot.
      */
-    SQUARE_PURPLE,
+    SQUARE_PURPLE(Color.MAGENTA),
     /**
      * A Red Square robot.
      */
-    SQUARE_RED,
+    SQUARE_RED(Color.RED),
     /**
      * A Yellow Square robot.
      */
-    SQUARE_YELLOW,
+    SQUARE_YELLOW(Color.YELLOW),
     /**
      * A Black Square robot.
      */
-    SQUARE_BLACK,
+    SQUARE_BLACK(Color.BLACK),
     /**
      * A White Square robot.
      */
-    SQUARE_WHITE;
+    SQUARE_WHITE(Color.WHITE);
+
+    /**
+     * The color of this robot family.
+     */
+    private final Color color;
+
+    /**
+     * Creates a new robot family with the given color.
+     *
+     * @param color the color of the robot family
+     */
+    RobotFamily(final Color color) {
+        this.color = color;
+    }
+
+    /**
+     * Returns the color of this robot family.
+     *
+     * @return the color of this robot family
+     */
+    public Color getColor() {
+        return color;
+    }
 
     /**
      * Returns the identifier of this robot family.
